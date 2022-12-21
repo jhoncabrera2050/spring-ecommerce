81d18bd5bb9b5e10bf649f46c381582d53360735
Jhon Cabrera <jhoncabrera2050@gmail.com>
2022-12-21 08:28:33 -0500

    crear las clases model,mapeo,tablas con JPA,integrado dieño de vistas

--- src/main/java/com/curso/ecommerce/model/DetalleOrden.java
+++ src/main/java/com/curso/ecommerce/model/DetalleOrden.java
@@ -1,5 +1,114 @@
 package com.curso.ecommerce.model;
 
-public class DetalleOrden {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "detalles")
+public class DetalleOrden {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nombre;
+	private double cantidad;
+	private double precio;
+	private double total;
+	private double total1;
+	@OneToOne
+	private Orden orden;
+	@ManyToOne
+	private Producto producto;
+	public DetalleOrden() {
+		
+	}
+	// creacion de un constructor para instancear 
+
+	public DetalleOrden(Integer id, String nombre, double cantidad, double precio, double total, double total1) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+		this.precio = precio;
+		this.total = total;
+		this.total1 = total1;
+	}
+	// creacion de sett y gett para seteo y obtencion de datos
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public double getTotal1() {
+		return total1;
+	}
+
+	public void setTotal1(double total1) {
+		this.total1 = total1;
+	}
+	
+
+	public Orden getOrden() {
+		return orden;
+	}
+
+	public void setOrden(Orden orden) {
+		this.orden = orden;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "DetalleOrden [id=" + id + ", nombre=" + nombre + ", cantidad=" + cantidad + ", precio=" + precio
+				+ ", total=" + total + ", total1=" + total1 + "]";
+	}
+	
 }
--- src/main/java/com/curso/ecommerce/model/Usuario.java
+++ src/main/java/com/curso/ecommerce/model/Usuario.java
@@ -1,6 +1,18 @@
 package com.curso.ecommerce.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String username;
@@ -10,10 +22,18 @@ public class Usuario {
 	private String tipo;
 	private String password;
 	
+	
+	//listado de productos nos devolcera
+	@OneToMany(mappedBy = "usuario")
+	private List<Producto> productos;
+	
+	//lista de ordenes para un usuario
+	@OneToMany(mappedBy = "usuario")
+	private List<Orden>ordenes;
 	public Usuario() {
 		
 	}
-	
+
 	public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono,
 			String tipo, String password) {
 		super();
@@ -74,6 +94,23 @@ public class Usuario {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+
+	public List<Orden> getOrdenes() {
+		return ordenes;
+	}
+
+	public void setOrdenes(List<Orden> ordenes) {
+		this.ordenes = ordenes;
+	}
+	
 	
 	
 }
