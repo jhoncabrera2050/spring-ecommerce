a66255ff27866ac94cedf6f02b049c053139fd43
Jhon Cabrera <jhoncabrera2050@gmail.com>
2022-12-23 21:04:53 -0500

    plantilla de editacion

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -1,10 +1,13 @@
 package com.curso.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,6 +21,7 @@ public class ProductoController {
 	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
 	@Autowired
 	private ProductoService productoService;
+	
 	@GetMapping("")
 	public String show(Model model) {
 		model.addAttribute("productos", productoService.findAll());
@@ -35,8 +39,19 @@ public class ProductoController {
 		productoService.save(producto);
 		return "redirect:/productos";
 	}
-	@GetMapping("/edit")
-	public String edit() {
+	  
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id, Model model) {
+		Producto producto = new Producto();
+		Optional<Producto> optionalProducto = productoService.get(id);
+		producto=optionalProducto.get();
+		LOGGER.info("producto buscado : {}",producto);
+		model.addAttribute("producto", producto);
 		return "productos/edit";
+	}
+	@PostMapping("/update")
+	public String update(Producto producto) {
+		productoService.update(producto);
+		return "redirect:/productos";
 	}
 }
